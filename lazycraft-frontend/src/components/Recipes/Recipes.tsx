4392e5723cbf0af68255cebac75b9c284c017689
Setzio <tellymartelaudion@gmail.com>
2022-12-13 19:18:04 +0100

    Feat: Routing basique et styles
    - Ajout des pages Items et Recipes
    - Mise en place d'un style simple

---
+++ lazycraft-frontend/src/components/Recipes/Recipes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Recipes.scss"
+
+export class Recipes extends React.Component <{}> {
+    render() {
+        return (
+            <h1 className="recipes-title">Welcome to Recipes</h1>
+        );
+    }
+}
